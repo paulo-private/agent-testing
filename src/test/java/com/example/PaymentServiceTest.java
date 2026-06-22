68ff36052d551979951d6c44a44dcb5c5c162e4f
Paulo Canelas <paulo.santos@sonarsource.com>
2026-06-22 11:54:46 +0200

    Add Java files to test S8714 and S1220/S2309 hints

---
+++ src/test/java/com/example/PaymentServiceTest.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class PaymentServiceTest {
+
+    @Test
+    void processPayment_shouldNotThrow() {
+        PaymentService service = new PaymentService();
+        try {
+            service.processPayment(100.0);
+        } catch (Exception e) {
+            fail("processPayment should not throw an exception");
+        }
+    }
+
+    @Test
+    void refundPayment_shouldNotThrow() {
+        PaymentService service = new PaymentService();
+        try {
+            service.refund(50.0);
+        } catch (Exception e) {
+            fail("refund should not throw an exception");
+        }
+    }
+}
